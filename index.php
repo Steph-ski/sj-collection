8db00cc4f52bfb5e604be41d09b8c3f9df7364f2
Steph-ski <stephaniejohns57@gmail.com>
2023-02-06 16:58:46 +0000

    end of day WIP

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ require_once 'functions.php';
 $db = createDbConn();
 $films = displayDb($db);
 
-echo '<pre>';
-var_dump($films);
-echo '</pre>';
+//echo '<pre>';
+//var_dump($films);
+//echo '</pre>';
 
 ?>
 
@@ -18,10 +18,25 @@ echo '</pre>';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="normalize.css"/>
     <link rel="stylesheet" type="text/css" href="styles.css"/>
-    <title>Portfolio Project - Steph</title>
+    <title>Disney Film Collection</title>
 </head>
 
 <body>
+
+<header>
+    <section class="overall">
+        <h1> Disney Film Collection</h1>
+
+    </section>
+
+</header>
+
+<main>
+    <section class="overall">
+
+    </section>
+
+</main>
 
 
 
