ff0669c4eca73f94f8fec2c2ffff3f4eb47b406e
Steph-ski <stephaniejohns57@gmail.com>
2023-02-07 12:09:30 +0000

    updated HTML & Css. fonts and images added

--- index.php
+++ index.php
@@ -24,20 +24,88 @@ $films = displayDb($db);
 <body>
 
 <header>
-    <section class="overall">
         <h1> Disney Film Collection</h1>
-
-    </section>
 
 </header>
 
 <main>
     <section class="overall">
+        <div class="film_collection">
+            <article>
+                <div class="film_info1">
+                    <img src="stock image.jpeg" alt="stock image">
+                    <h2> Film Title </h2>
+                </div>
+                <div class="film_info2">
+                    <p> Year of Release: </p>
+                    <p> Main Character: </p>
+                    <p> Rating out of 10: </p>
+                </div>
+            </article>
+
+            <article>
+                <div class="film_info1">
+                    <img src="stock image.jpeg" alt="stock image">
+                    <h2> Film Title </h2>
+                </div>
+                <div class="film_info2">
+                    <p> Year of Release: </p>
+                    <p> Main Character: </p>
+                    <p> Rating out of 10: </p>
+                </div>
+            </article>
+
+            <article>
+                <div class="film_info1">
+                    <img src="stock image.jpeg" alt="stock image">
+                    <h2> Film Title </h2>
+                </div>
+                <div class="film_info2">
+                    <p> Year of Release: </p>
+                    <p> Main Character: </p>
+                    <p> Rating out of 10: </p>
+                </div>
+            </article>
+
+            <article>
+                <div class="film_info1">
+                    <img src="stock image.jpeg" alt="stock image">
+                    <h2> Film Title </h2>
+                </div>
+                <div class="film_info2">
+                    <p> Year of Release: </p>
+                    <p> Main Character: </p>
+                    <p> Rating out of 10: </p>
+                </div>
+            </article>
+
+            <article>
+                <div class="film_info1">
+                    <img src="stock image.jpeg" alt="stock image">
+                    <h2> Film Title </h2>
+                </div>
+                <div class="film_info2">
+                    <p> Year of Release: </p>
+                    <p> Main Character: </p>
+                    <p> Rating out of 10: </p>
+                </div>
+            </article>
+
+        </div>
+
 
     </section>
 
 </main>
+<section class="button_section">
+    <div class="add_button">
+        <a  href="#">Click Here to Add to Collection</a>
+    </div>
+</section>
 
+<footer>
+
+</footer>
 
 
 </body>
