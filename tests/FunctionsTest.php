02225f0d3f047291b266bfc38409439ed2364ef5
Steph-ski <stephaniejohns57@gmail.com>
2023-02-07 15:17:20 +0000

    tests folder & file recreated

---
+++ tests/FunctionsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'functions.php';
+
+use PHPUnit\Framework\TestCase;
+
+class FunctionsTest extends TestCase
+{
+    public function testSuccessDisplayFilms()
+    {
+        
+    }
+}
